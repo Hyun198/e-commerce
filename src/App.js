31c6f28b801025770488853469291f9143d6b617
Hyun <hwn123h@naver.com>
2024-08-19 23:58:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ProductAll from './pages/ProductAll'
+import ProductDetail from './pages/ProductDetail'
+import Login from './pages/Login'
+import Navbar from './components/Navbar/Navbar';
 
+//1. 전체상품
+//2. 로그인페이지
+//3. 상품디테일페이지
+//4. 로그아웃기능
+//5. 마이페이지
+//6. 장바구니페이지
+//7. 상품 검색 페이지
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/product/:id' element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Navbar.style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+const Navbar = () => {
+    const categories = [
+        "여성",
+        "Divided",
+        "남성",
+        "유아",
+        "아동",
+        "Sale",
+        "지속가능성",
+    ]
+    return (
+        <div className="navbar">
+            <div className="login">
+                <FontAwesomeIcon icon={faUser} />
+                <div>Login</div>
+            </div>
+            <div className="logo">
+                <img src="hm.png" width="120" height="100" />
+            </div>
+            <div className="category">
+                <ul>
+                    {categories.map((category) => (
+                        <li>{category}</li>
+                    ))}
+                </ul>
+                <div className="search-bar">
+                    <input type="text" placeholder='Search...' />
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
