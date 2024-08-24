e5eb96267f977ca45543728718afe7270453eb00
Hyun <hwn123h@naver.com>
2024-08-24 21:43:43 +0900

    반응형 사이드바

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.style.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faX, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { useSelector, useDispatch } from 'react-redux'
 import { authenticateAction } from '../../redux/actions/authenticateAction'
 
 
 const Navbar = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const toggleSlider = () => {
+        setIsSidebarOpen(!isSidebarOpen)
+    }
 
     const navigate = useNavigate();
     const categories = [
@@ -35,6 +39,9 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
+            <div className="menu" onClick={toggleSlider}>
+                <FontAwesomeIcon icon={isSidebarOpen ? faX : faBars} />
+            </div>
             <div className="login">
                 <FontAwesomeIcon icon={faUser} />
                 {authenticate === true ? <div className="Logout" onClick={logout}><span>Logout</span> </div> : <Link to="/login"><span>Login</span></Link>}
@@ -52,6 +59,18 @@ const Navbar = () => {
                     <input type="text" placeholder='Search...' onKeyPress={search} />
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </div>
+            </div>
+            {/* 사이드바 (모바일에서만 보임) */}
+            <div className={`sidebar ${isSidebarOpen ? 'active' : ''}`}>
+                <div className="search-bar">
+                    <input type="text" placeholder='Search...' onKeyPress={search} />
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+                </div>
+                <ul>
+                    {categories.map((category, index) => (
+                        <li key={index}># {category}</li>
+                    ))}
+                </ul>
 
             </div>
 
@@ -59,4 +78,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar 
